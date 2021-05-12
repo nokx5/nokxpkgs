9836c7aa7ef1c33e7a8082a81a73f2539597e852
nokx <72279573+nokx5@users.noreply.github.com>
2021-05-12 14:03:07 +0200

    update nixpkgs branch since we will use flakes at some point

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ let
     # example source: https://github.com/nixos/nixpkgs/archive/c00959877fb06b09468562518b408acda886c79e.tar.gz";
     # - get the SHA256 hash using: nix-prefetch-url --unpack <url>
     url =
-      "https://github.com/NixOS/nixpkgs/archive/eb7e1ef185f6c990cda5f71fdc4fb02e76ab06d5.tar.gz";
-    sha256 = "1ibz204c41g7baqga2iaj11yz9l75cfdylkiqjnk5igm81ivivxg";
+      "https://github.com/NixOS/nixpkgs/archive/8a0c5da648702f4620e3a2926f48b38dd1d86562.tar.gz";
+    sha256 = "0y84yknvgkkchkdpydh7klq4kfi73mzb5g7830xd17gzjj5chav9";
   };
 
 in import pinned-pkgs { overlays = (map (x: import x) [ ./nixpkgs-nokx ]); }
